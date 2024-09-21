b2d9e88f6ba1880c7c547afc409a6423297a7647
GirishCodeAlchemy <venkatareddygirish@gmail.com>
2024-09-20 21:45:53 -0400

    Fix the setup.py version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="codealchemy",
-    version="0.1",
+    version="1.0",
     packages=find_packages(),
     install_requires=[],
     author="GirishCodeAlchemy",
