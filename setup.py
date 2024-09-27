761b005786e0bcf0022361a016e6d60ea6102872
GirishCodeAlchemy <venkatareddygirish@gmail.com>
2024-09-26 23:02:32 -0400

    Bumped the version to 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="codealchemy",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     install_requires=[],
     author="GirishCodeAlchemy",
