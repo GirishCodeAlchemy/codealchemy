11015b047b9368cd48106d4b9631ac6144202801
GirishCodeAlchemy <venkatareddygirish@gmail.com>
2024-11-13 23:37:26 -0500

    Updated the codealchemy version to 1.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="codealchemy",
-    version="1.2.1",
+    version="1.2.2",
     packages=find_packages(),
     install_requires=[
         "confluent-kafka==2.6.0",
